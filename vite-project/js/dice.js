736e4db3b28cf829fe362b1fb408b6ef89a0f930
isaacl81 <isaaclin2007@gmail.com>
2024-01-12 13:43:01 -0500

    start

---
+++ vite-project/js/dice.js
@@ -0,0 +1,26 @@
+export const dice = [
+    {
+        value: 1,
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Dice-1-b.svg/640px-Dice-1-b.svg.png",
+    },
+    {
+        value: 2,
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Dice-2-b.svg/640px-Dice-2-b.svg.png",
+    },
+    {
+        value: 3,
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Dice-3-b.svg/640px-Dice-3-b.svg.png",
+    },
+    {
+        value: 4,
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Dice-4-b.svg/640px-Dice-4-b.svg.png",
+    },
+    {
+        value: 5,
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Dice-5-b.svg/640px-Dice-5-b.svg.png",
+    },
+    {
+        value: 6,
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Dice-6-b.svg/640px-Dice-6-b.svg.png",
+    },
+]
