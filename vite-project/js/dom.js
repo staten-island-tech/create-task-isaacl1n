2b3464d6a1eefe4fdf1204cb1a30059a0b53f206
isaacl81 <isaaclin2007@gmail.com>
2024-01-17 11:34:41 -0500

    complete

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,3 +1,6 @@
 export const DOMselectors = {
-    
+  rollForm: document.getElementById("roll"),
+  rollOptions: document.getElementById("options"),
+  outcomes: document.getElementById("outcomes"),
+  totalValue: document.getElementById("total-value"),
 };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,3 +2,38 @@ import '../css/style.css'
 import { DOMselectors } from './dom.js'
 import { dice } from './dice.js'
 
+function rollDice(diceRolled) {
+  const rolls = []
+  for(let i = 0; i < diceRolled; i++) {
+    const randomValue = dice[
+      Math.floor(Math.random() * dice.length)
+    ];
+    rolls.push(randomValue);
+  };
+  return rolls;
+};
+
+function addRolls(outcomes) {
+  return outcomes.reduce((accumulator, roll) => 
+    accumulator + roll.value, 0
+  );
+};
+
+DOMselectors.rollForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  DOMselectors.outcomes.innerHTML = "";
+  const diceRolled = parseInt(DOMselectors.rollOptions.value);
+  const outcomes = rollDice(diceRolled);
+  outcomes.forEach(roll => {
+    DOMselectors.outcomes.insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="dice">
+          <img src="${roll.image}" alt="You rolled a ${roll.value}." class="dice-image"><br>
+          <h3 class="dice-value">${roll.value}</h3>
+        </div>
+      `
+    );
+  });
+  DOMselectors.totalValue.innerHTML = `Total Sum: ${addRolls(outcomes)}`
+});
